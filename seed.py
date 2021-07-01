327ee616e3c38c289ee1b74ed0fcc171e91eea11
zataara <bryantmac@gmail.com>
2021-06-30 22:15:33 -0600

    added new pets to db

--- seed.py
+++ seed.py
@@ -10,3 +10,19 @@ db.create_all()
 #If table isn't empty, empty it
 Pet.query.delete()
 
+#Create some new pets
+Fido = Pet(name='Fido', species='Terrier')
+Spot = Pet(name='Spot', species='Beagle')
+Tucker = Pet(name='Tukcker', species='Cockapoo')
+Tywin = Pet(name='Tywin', species='Belgian Shepher')
+Lyra = Pet(name='Lyra', species='Calico')
+
+#Move new pets to session
+db.session.add(Fido)
+db.session.add(Spot)
+db.session.add(Tucker)
+db.session.add(Tywin)
+db.session.add(Lyra)
+
+#Commit new pets
+db.session.commit()

beeead8a02a508a1b7dd6e0d3679c00563da6a8d
zataara <bryantmac@gmail.com>
2021-07-12 10:48:39 -0700

    updating form data

--- app.py
+++ app.py
@@ -24,12 +24,18 @@ def root():
     return render_template('index.html', pets=pets)
 
 #
-@app.route('/add')
+@app.route('/add', methods=['GET', 'POST'])
 def add_pet():
     '''Page to add a new pet to the adoption agency'''
+    
+    form = AddPetForm()
+    
     if form.validate_on_submit():
         name = form.name.data
         species = form.species.data
         photo_url = form.photo_url.data
         age = form.age.data
         notes = form.notes.data
+        return redirect('/add')
+    else:
+        return render_template('add.html', form=form)

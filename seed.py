fd6025d5d4d3f6533b2a147de6d9d21460ed7630
zataara <bryantmac@gmail.com>
2021-07-13 01:21:39 -0700

    fixed issues with seed data

--- seed.py
+++ seed.py
@@ -1,6 +1,6 @@
 '''Seed file to make samaple data for Users Database'''
 
-from models import User, Post, Tag, PostTag, db
+from models import Pet, db
 from app import app
 
 #Create all tables
@@ -11,9 +11,9 @@ db.create_all()
 Pet.query.delete()
 
 #Create some new pets
-Fido = Pet(name='Fido', species='Terrier', age=4, photo_url='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.thesprucepets.com%2Fterrier-dog-breeds-5112878&psig=AOvVaw2a9GJ9-3GA1j7bOdcloZ8L&ust=1625715349389000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCNjxnuGD0PECFQAAAAAdAAAAABAE')
-Spot = Pet(name='Spot', species='Beagle', age=1, photo_url='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.dogzone.com%2Fbreeds%2Fbeagle%2F&psig=AOvVaw2jSg5c_N2F48Z3KNS5DflN&ust=1625715312424000&source=images&cd=vfe&ved=0CAoQjRxqFwoTCOjL0c-D0PECFQAAAAAdAAAAABAJ')
-Tucker = Pet(name='Tukcker', species='Cockapoo', age=7, photo_url='https://www.instagram.com/p/Bt1t1FAnEPY/')
+Fido = Pet(name='Fido', species='Terrier', age=4, photo_url='https://www.thesprucepets.com/thmb/MFsTgJGoYjjhpLz66NO8Fp-8JxE=/960x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/Westie-74f76b3f57e643639d67298018374e39.jpg')
+Spot = Pet(name='Spot', species='Beagle', age=1, photo_url='https://www.dogzone.com/images/breeds/beagle.jpg')
+Tucker = Pet(name='Tucker', species='Cockapoo', age=7, photo_url='https://www.instagram.com/p/Bt1t1FAnEPY/?utm_source=ig_embed&amp;utm_campaign=loading')
 Tywin = Pet(name='Tywin', species='Belgian Shepher', age=6, photo_url='https://www.instagram.com/p/BcGQEpNF35M/')
 Lyra = Pet(name='Lyra', species='Calico', age=1, photo_url='https://www.instagram.com/p/CG3YDdLFNPNTcOCOaqQ5xsfXzGmqcxcLl8OYjU0/')
 

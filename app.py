c737d0357f0b2b7109e1109f36bcac78a97f2130
zataara <bryantmac@gmail.com>
2021-09-03 04:25:22 -0700

    updating new environment variables

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ from flask import Flask, request, render_template, redirect, flash, session, url
 from flask_debugtoolbar import DebugToolbarExtension
 from models import db, connect_db, Pet
 from forms import AddPetForm, EditPetForm
+import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///adopt-a-pet'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'postgresql:///adopt-a-pet')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
-app.config['SECRET_KEY'] = 'supersecretkey'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'supersecretkey')
 app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
-debug = DebugToolbarExtension(app)
+
 
 
 connect_db(app)
